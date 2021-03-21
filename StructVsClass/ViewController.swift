9276fad03d68fa1edc441f7ef7ffdc06b53156d0
HassanAfnan <hassanafnan20@gmail.com>
2021-03-22 01:10:52 +0500

    Struct vs class

--- StructVsClass/ViewController.swift
+++ StructVsClass/ViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    struct MyStruct {
+        var name: String
+        
+        init(name: String) {
+            self.name = name
+        }
+    }
+    
+    class MyClass {
+        var name: String
+        
+        init(name: String) {
+            self.name = name
+        }
+    }
+    
+    func forStruct (){
+        let fName = MyStruct(name: "Afnan")
+        var sName = fName
+        sName.name = "Hassan"
+        print(fName.name)
+        print(sName.name)
+    }
 
+    func forClass() {
+        let fName = MyClass(name: "IOS Swift")
+        var sName  = fName
+        sName.name = "Objective C"
+        print(fName.name)
+        print(sName.name)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        forStruct()
+        forClass()
     }
 
 
