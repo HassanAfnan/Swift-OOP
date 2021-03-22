0bf66d1ecae88bdfdd615cc3442ee4d39fa64166
HassanAfnan <hassanafnan20@gmail.com>
2021-03-23 01:41:56 +0500

    Initial Commit

---
+++ Generics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Generics
+//
+//  Created by Afnan on 23/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
